b8df668dc33d913b79c5b0303995961d0205fbd4
Rakhymzhan_N <rakhymjhan.n@mybuh.kz>
2023-05-31 10:54:23 +0600

    Add RestaurantScreen and RestaurantList

---
+++ src/components/RestaurantScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RestaurantList from './RestaurantList';
+
+const RestaurantScreen = () => {
+  return (
+    <div>
+      <h1>RestaurantScreen</h1>
+      <RestaurantList />
+    </div>
+  );
+};
+
+export default RestaurantScreen;
